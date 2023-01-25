56cc2424b1400420488a7bc4e30a0da3a2b99154
ngm01 <nathaniel.g.moore@gmail.com>
2023-01-24 23:31:33 -0500

    set up barebones express server

--- cleanUp.js
+++ cleanUp.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
-const words_dictionary = require('./words_dictionary.json');
-const words_array = require('./words_array.json');
+// const words_dictionary = require('./words_dictionary.json');
+// const words_array = require('./words_array.json');
 
-const raw_words = Object.keys(words_dictionary);
+//const raw_words = Object.keys(words_dictionary);
 
 // quick-and-dirty attempt to remove plurals while maintaining
 // singular forms that end in -s, e.g. 'grass', 'chess' etc
@@ -25,11 +25,21 @@ const hasSevenUniqueChars = (word) => {
     // else return false
 }
 
+/*
 const wordle_words = raw_words.filter(word => word.length === 5).filter(removePlural);
-writeToFile('./wordle_clone_words.json', JSON.stringify(wordle_words));
+writeToFile('./wordle_clone_words_old.json', JSON.stringify(wordle_words));
 
 const spelling_bee_base_words = words_array.filter(word => word.length >= 7).filter(hasSevenUniqueChars);
 writeToFile('./spelling_bee_clone_base_words.json', JSON.stringify(spelling_bee_base_words));
+*/
+
+const allowed_answers_array = fs.readFileSync('wordle_allowed_answers_plain.txt').toString().toUpperCase().split("\n");
+const allowed_guesses_array = fs.readFileSync('wordle_allowed_guesses_plain.txt').toString().toUpperCase().split("\n");
+const final_guesses = allowed_guesses_array.concat(allowed_answers_array).sort();
+
+writeToFile('./allowed_guesses.json', JSON.stringify(final_guesses));
+writeToFile('./allowed_answers.json', JSON.stringify(allowed_answers_array));
+
 
 function writeToFile(file_name, json) {
     try {
--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-const wordle_words = require('./wordle_clone_words.json');
+const express = require('express');
+const answers = require('./allowed_answers.json');
 
-console.log(wordle_words.length);
+const app = express();
+app.use()
+const port = 5000;
+let today = new Date().toDateString();
+let todaysWord = "";
+
+function getTodaysWord() {
+    dateCheck = new Date().toDateString();
+    const rando = Math.floor(Math.random() * (answers.length - 0) + 0)
+    if(dateCheck !== today || todaysWord === "") {
+        today = dateCheck;
+        todaysWord = answers[rando];
+    }
+    return todaysWord;
+}
+
+app.get('/word', (req, res) => {
+    res.set({
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+    });
+    res.json({word: getTodaysWord()})
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
